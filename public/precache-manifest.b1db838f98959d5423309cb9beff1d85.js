5962227a40799b58101e37d627ba71b154ae5cf5
Osmo Leppäniemi <oleppani@gmail.com>
2021-03-05 12:16:56 +0200

    uusi

---
+++ public/precache-manifest.b1db838f98959d5423309cb9beff1d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9a7da5090cbea75ab0767d2d7c0b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44fa7d273c1f2d4bc5b2",
+    "url": "/static/css/main.5425005d.chunk.css"
+  },
+  {
+    "revision": "ae6ca2c91c68681a5144",
+    "url": "/static/js/2.51726e3a.chunk.js"
+  },
+  {
+    "revision": "3f528736a4a9660bbca7ea4ebd01657e",
+    "url": "/static/js/2.51726e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fa7d273c1f2d4bc5b2",
+    "url": "/static/js/main.5adfb31d.chunk.js"
+  },
+  {
+    "revision": "b7794f40c40311e06b2e",
+    "url": "/static/js/runtime-main.86010d0d.js"
+  }
+]);
